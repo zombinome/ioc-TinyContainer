27cb47423c62725db46b21082b644a95b7f533db
Alexey Rubanovsky <alexey.rubanovsky@noveogroup.com>
2019-08-08 10:32:02 +0300

    Add TypeHelper class

---
+++ TinyContainer/TypeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace TinyContainer
+{
+    internal static class TypeHelper
+    {
+        public static ConstructorInfo GetConstructor(Type type)
+        {
+            return type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)[0];
+        }
+
+        public static object CreateInstance(TinyContainer container, ConstructorInfo constructor)
+        {
+            var parameters = constructor.GetParameters();
+            object[] args = parameters.Select(p => container.Resolve(p.ParameterType)).ToArray();
+
+            return constructor.Invoke(args);
+        }
+    }
+}
