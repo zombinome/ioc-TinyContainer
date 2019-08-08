a3b064308a3b5dbc57c784dab90ccd9db41df7fe
Alexey Rubanovsky <alexey.rubanovsky@noveogroup.com>
2019-08-08 10:29:26 +0300

    Add Resolve method to TinyContainer

---
+++ TinyContainer/FailedToResolveDependencyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TinyContainer
+{
+    [Serializable]
+    internal class FailedToResolveDependencyException : Exception
+    {
+        public FailedToResolveDependencyException(Type serviceType, string message, Exception innerException = null): base(message, innerException)
+        {
+            this.ServiceType = serviceType;
+        }
+
+        public Type ServiceType { get; set; }
+
+    }
+}
--- TinyContainer/TinyContainer.cs
+++ TinyContainer/TinyContainer.cs
@@ -7,9 +7,30 @@ namespace TinyContainer
     {
         private readonly Dictionary<Type, ILifetimeManager> registrations = new Dictionary<Type, ILifetimeManager>();
 
+        private readonly Dictionary<object, ILifetimeManager> objectsToRelease = new Dictionary<object, ILifetimeManager>();
+
         public void Register(Type dependencyType, ILifetimeManager lifetimeManager)
         {
             registrations[dependencyType] = lifetimeManager;
+        }
+
+        public object Resolve(Type serviceType)
+        {
+            if (registrations.TryGetValue(serviceType, out ILifetimeManager lifetimeManager))
+            {
+                var dependency = lifetimeManager.Resolve(this, out bool shouldRelease);
+                if (dependency != null)
+                {
+                    if (shouldRelease)
+                    {
+                        objectsToRelease.Add(dependency, lifetimeManager);
+                    }
+
+                    return dependency;
+                }
+            }
+
+            throw new FailedToResolveDependencyException(serviceType, "Unknown service type. Unable to resolve.");
         }
 
         public void Dispose()
