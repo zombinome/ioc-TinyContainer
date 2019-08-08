1cbf0b9b43a544760de339f16971d7156670c461
Alexey Rubanovsky <alexey.rubanovsky@noveogroup.com>
2019-08-08 10:15:20 +0300

    Add ILifetimeManager interface and TinyContainer class with Regsiter method implementation

---
+++ TinyContainer/ILifetimeManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TinyContainer
+{
+    public interface ILifetimeManager : IDisposable
+    {
+        object Resolve(TinyContainer container, out bool shoudRelease);
+
+        void Release(object dependency);
+    }
+}
---
+++ TinyContainer/TinyContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TinyContainer
+{
+    public class TinyContainer: IDisposable
+    {
+        private readonly Dictionary<Type, ILifetimeManager> registrations = new Dictionary<Type, ILifetimeManager>();
+
+        public void Register(Type dependencyType, ILifetimeManager lifetimeManager)
+        {
+            registrations[dependencyType] = lifetimeManager;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
