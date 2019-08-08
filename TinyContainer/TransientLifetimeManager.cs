ad4a82acc95f07d388aca9ebb21176691dd617cb
Alexey Rubanovsky <alexey.rubanovsky@noveogroup.com>
2019-08-08 10:33:55 +0300

    Add transient lifetime manager

---
+++ TinyContainer/TransientLifetimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace TinyContainer
+{
+    public class TransientLifetimeManager : ILifetimeManager
+    {
+        protected readonly Type dependencyType;
+
+        protected readonly bool isDisposable;
+
+        protected readonly ConstructorInfo constructor = null;
+
+        public TransientLifetimeManager(Type serviceType)
+        {
+            dependencyType = serviceType;
+            constructor = TypeHelper.GetConstructor(serviceType);
+            isDisposable = typeof(IDisposable).IsAssignableFrom(serviceType);
+        }
+
+        public object Resolve(TinyContainer container, out bool shouldRelease)
+        {
+            shouldRelease = isDisposable;
+            return TypeHelper.CreateInstance(container, constructor);
+        }
+
+        public void Release(object dependency)
+        {
+            if (dependency is IDisposable)
+            {
+                ((IDisposable)dependency).Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
